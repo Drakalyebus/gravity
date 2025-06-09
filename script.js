546ecca223b5cb2d00891e45ae51a86aa98f21c2
Drakalyebus <mishastelephonom@gmail.com>
2025-06-09 22:52:59 +0300

    follow and gravity field

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-ctx.fillStyle = "black";
+ctx.fillStyle = `rgb(${255 / 2}, ${255 / 2}, ${255 / 2}, 1)`;
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 const G = 9.8;
@@ -14,6 +14,8 @@ let scale = 1;
 let isDragging = false;
 let relocated = false;
 let collision = false;
+let maxForce = 0;
+let followIds = [];
 
 collisionCheckbox.addEventListener('change', () => {
     collision = collisionCheckbox.checked;
@@ -34,7 +36,7 @@ class Vector {
         this.y *= scalar;
         return this;
     }
-    substract(vector) {
+    subtract(vector) {
         this.x -= vector.x;
         this.y -= vector.y;
         return this;
@@ -67,11 +69,16 @@ let objects = [];
 canvas.addEventListener('click', e => {
     if (!relocated) {
         const mass = +prompt("Enter the mass of the object", '1');
+        const radius = Math.sqrt(Math.abs(mass) / Math.PI) * 10;
+        const localMaxForce = G * mass / radius ** 2;
+        if (localMaxForce > maxForce) {
+            maxForce = localMaxForce;
+        }
         objects.push({
-            x: (e.offsetX - offsetX) / scale - mass * 10 / 5,
+            x: (e.offsetX - offsetX) / scale,
             y: (e.offsetY - offsetY) / scale,
             delta: Vector.random().multiply(0.1 / mass),
-            radius: Math.abs(mass * 10),
+            radius,
             mass,
             color: `hsl(${(360 / 10 * objects.length) % 360}, 100%, 50%)`,
             path: []
@@ -107,7 +114,7 @@ function gravity() {
                         const impulseVector = new Vector(direction.x * impulse, direction.y * impulse);
                         const objectImpulse = new Vector(impulseVector.x * other.mass, impulseVector.y * other.mass);
                         const otherImpulse = new Vector(impulseVector.x * object.mass, impulseVector.y * object.mass);
-                        object.delta.substract(objectImpulse);
+                        object.delta.subtract(objectImpulse);
                         other.delta.add(otherImpulse);
                     }
                 }
@@ -126,9 +133,40 @@ function gravity() {
 
 function draw() {
     ctx.setTransform(scale, 0, 0, scale, offsetX, offsetY);
+    if (followIds.length > 0) {
+        const midX = followIds.reduce((acc, id) => acc + objects[id].x, 0) / followIds.length;
+        const midY = followIds.reduce((acc, id) => acc + objects[id].y, 0) / followIds.length;
+        const maxDistance = Math.max(...followIds.map(id => Math.hypot(midX - objects[id].x, midY - objects[id].y) + objects[id].radius));
+        const minScreen = Math.min(canvas.width, canvas.height);
+        if (followIds.length > 1) {
+            scale = Math.min(minScreen / maxDistance / 2, scale);
+        }
+        offsetX = canvas.width / 2 - midX * scale;
+        offsetY = canvas.height / 2 - midY * scale;
+    }
 
-    ctx.fillStyle = "rgb(0, 0, 0, 1)";
+    ctx.fillStyle = `rgb(${255 / 2}, ${255 / 2}, ${255 / 2}, 1)`;
     ctx.fillRect(-offsetX / scale, -offsetY / scale, canvas.width / scale, canvas.height / scale);
+
+    const resolution = 20 / scale;
+    const sizeX = canvas.width * 2 / scale;
+    const sizeY = canvas.height * 2 / scale;
+    for (let x = 0; x < sizeX; x += resolution) {
+        for (let y = 0; y < sizeY; y += resolution) {
+            const realX = x - offsetX / scale;
+            const realY = y - offsetY / scale;
+            let totalForce = 0;
+            objects.forEach(object => {
+                const direction = new Vector(realX - object.x, realY - object.y);
+                const distance = Math.max(direction.length() - (object.radius), object.radius);
+                const force = distance > 0 ? G * object.mass / distance ** 2 : 0;
+                totalForce += force;
+            });
+            totalForce = Math.max(0, Math.min((totalForce / maxForce / 2 + 0.5) * 255, 255));
+            ctx.fillStyle = `rgb(${totalForce}, ${totalForce}, ${totalForce})`;
+            ctx.fillRect(realX - resolution / 2, realY - resolution / 2, resolution, resolution);
+        }
+    }
 
     objects.sort((a, b) => b.mass - a.mass).forEach(object => {
         ctx.beginPath();
@@ -159,6 +197,21 @@ function draw() {
 draw();
 
 let startX, startY;
+
+canvas.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+    let id = objects.findIndex(object => {
+        const distance = Math.hypot((e.offsetX - offsetX) / scale - object.x, (e.offsetY - offsetY) / scale - object.y);
+        return distance < object.radius;
+    });
+    if (id !== -1) {
+        if (followIds.includes(id)) {
+            followIds.splice(followIds.indexOf(id), 1);
+        } else {
+            followIds.push(id);
+        }
+    }
+});
 
 canvas.addEventListener("mousedown", (e) => {
     isDragging = true;
@@ -199,4 +252,9 @@ canvas.addEventListener("wheel", (e) => {
     // Корректируем смещение так, чтобы приближение было относительно курсора
     offsetX = mouseX - worldX * scale;
     offsetY = mouseY - worldY * scale;
+});
+
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 });
